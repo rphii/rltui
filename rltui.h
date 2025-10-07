bfbf52b25ddd48371f2258a0207fcd0fcca348c5
rphii <raphael.seitz18@gmail.com>
2025-10-07 20:05:36 +0000

    fix headers

--- rltui.h
+++ rltui.h
@@ -1,12 +1,17 @@
 #ifndef RLTUI_H
 
+#include "rltui/tui-buffer.h"
+#include "rltui/tui-cell.h"
+#include "rltui/tui-color.h"
 #include "rltui/tui-die.h"
 #include "rltui/tui-esc-code.h"
+#include "rltui/tui-fx.h"
 #include "rltui/tui-global.h"
 #include "rltui/tui-input.h"
 #include "rltui/tui-point.h"
 #include "rltui/tui-raw.h"
 #include "rltui/tui-rect.h"
+#include "rltui/tui-screen.h"
 #include "rltui/tui-write.h"
 #include "rltui/tui.h"
 
