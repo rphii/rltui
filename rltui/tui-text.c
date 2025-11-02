87ad0b854757e4a5957c335c51b2e495a811b8e4
rphii <raphael.seitz18@gmail.com>
2025-11-02 15:00:30 +0000

    fix pop issues

--- rltui/tui-text.c
+++ rltui/tui-text.c
@@ -35,11 +35,12 @@ So_Uc_Point tui_text_line_pop(Tui_Text_Line *tx) {
     if(so_len(tx->so)) {
         size_t i = so_len(tx->so);
         while(so_len(tx->so)) {
-            unsigned char c = so_at(tx->so, so_len(tx->so));
+            unsigned char c = so_at(tx->so, so_len(tx->so) - 1);
             --i;
             if(!(c & 0x80)) break;
         }
         so_uc_point(so_i0(tx->so, i), &result);
+        tx->so = so_iE(tx->so, i);
         tx->visual_len -= rlwcwidth(result.val);
     }
     return result;
